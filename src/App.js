c1bd95b566277a6d77d743c746af1a7b7f36f618
riya goyal <goyalriya02@gmail.com>
2019-12-06 18:12:48 +0530

    State: Added state in ‘AddSubscriber’ and ‘App’ components

--- src/App.js
+++ src/App.js
@@ -5,21 +5,18 @@ import './App.css';
 
 class App extends Component {
   
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      subscribersListToShow: []
+    }
+  }
 
-    let subsrcibers = [
-      {
-      id: 1,
-      name: "Shilpa Bhat",
-      phone: "8888888888"
-      },
-      {
-        id: 2,
-        name: "Srishti Gupta",
-        phone: "9999999999"
-        }
-    ];
+  render(){
+
+  
   return (
+
    <div>
    
    <Header heading="Phone directory" />
@@ -32,7 +29,8 @@ class App extends Component {
           </div>
 
           {
-            subsrcibers.map(sub => {
+           
+           this.state.subscribersListToShow.map(sub => {
               return <div key={sub.id} className="grid-container">
                  <span className="grid-item">{sub.name}</span>
                 <span className="grid-item">{sub.phone}</span>
